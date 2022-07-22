e53b87943ec203091708020022e41c02f78fea2e
21035849 <21035849@10.174.124.15>
2022-07-22 15:10:49 +0800

    added space

--- ResourceCentre_Refactor/src/Chromebook.java
+++ ResourceCentre_Refactor/src/Chromebook.java
@@ -9,7 +9,7 @@ public class Chromebook extends Item{
 
 	public String getOs() {
 		return os;
-	}  
+	}
 	
 	public String toString(){
 		
@@ -17,8 +17,8 @@ public class Chromebook extends Item{
 		String output = super.toString();
 		output += String.format("%-63s %-20d", output, getOs());
 		
-		return output;
+		return output; 
 	}
-}
+} 
 
 
