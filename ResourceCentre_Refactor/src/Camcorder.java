5d0b7822ee1317514dd611b36e8e4e18e5b098ea
21031099 <21031099@DESKTOP-HN72Q0P>
2022-07-29 11:54:22 +0800

    Daren modified line 2

--- ResourceCentre_Refactor/src/Camcorder.java
+++ ResourceCentre_Refactor/src/Camcorder.java
@@ -1,4 +1,5 @@
 //testing the commit in camcorder -Jun Yan
+//testing the commit in camorder - Daren Lee
 public class Camcorder extends Item{
 	private int opticalZoom;
 
@@ -12,7 +13,6 @@ public class Camcorder extends Item{
 	}
 	
 	public String toString(){
-		
 		// Write your codes here
 		String output = super.toString();
 		output += String.format("%-63s %-20d", output, opticalZoom);
